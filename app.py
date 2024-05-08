0b1da24858605bcea293be4ac6f54eb710662842
Toby-byte <tobiasbogenielsen@gmail.com>
2024-05-08 11:02:49 +0200

    hook_test_1

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-# 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
-
-# https://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@github.com/Toby-byte/web_dev.git
-
 from bottle import default_app, get, post, run
 import git
  
@@ -17,7 +13,7 @@ def git_update():
 ##############################
 @get("/")
 def _():
-  return "Three"
+  return "three"
  
 ##############################
 try:
